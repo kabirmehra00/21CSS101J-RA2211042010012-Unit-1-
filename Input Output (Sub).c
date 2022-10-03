94fc34f6e5e2f613de2462c80410be36ddb6fb00
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-03 21:13:53 +0530

    Create Input Output (Sub).c

---
+++ Input Output (Sub).c
@@ -0,0 +1,11 @@
+int main() {
+  int x, y;
+  int mul=x*y;
+  printf("Enter the value of x :\n", x);
+  scanf("%d", &x);
+  printf("Enter the value of y :\n", y);
+  scanf("%d", &y);
+  printf("The required value is :%d\n", mul=x*y);
+  scanf("%d", &mul);
+  return 0;
+}
