72a970712b2d7f658596676b09a205dc610df8e9
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-03 10:23:15 +0530

    Create Int,Float,Char.c

---
+++ Int,Float,Char.c
@@ -0,0 +1,10 @@
+main()
+{
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    char myLetter = 'D';
+  
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+}
