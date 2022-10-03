310db98cb922f2a1dc7ae759df8f77826cd6877a
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-03 11:03:51 +0530

    Create Subtract.c

---
+++ Subtract.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x , y;
+    int result = x + y;
+    printf("Enter the value of x :\n",x );
+    scanf("%d", &x);
+    printf("Enter the value of x :\n",y );
+    scanf("%d", &y);
+    printf("The required sum is :%d\n", result = x + y);
+    scanf("%d", &sum);
+    return 0;
+}
