afb6797ad79740fb2fc218a2220a4785329bb3de
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-03 21:17:28 +0530

    Create Input Output (Sub).c

---
+++ Input Output (Sub).c
@@ -0,0 +1,11 @@
+int main() {
+  int x, y;
+  int sub=x-y;
+  printf("Enter the value of x :\n", x);
+  scanf("%d", &x);
+  printf("Enter the value of y :\n", y);
+  scanf("%d", &y);
+  printf("The required value is :%d\n", sub=x-y);
+  scanf("%d", &sub);
+  return 0;
+}
