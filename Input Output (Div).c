5fcbda3ffe44c1ff90a5e8c37ad57df2510fcecb
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-03 21:18:48 +0530

    Create Input Output (Div).c

---
+++ Input Output (Div).c
@@ -0,0 +1,11 @@
+int main() {
+  int x, y;
+  int div=x/y;
+  printf("Enter the value of x :\n", x);
+  scanf("%d", &x);
+  printf("Enter the value of y :\n", y);
+  scanf("%d", &y);
+  printf("The required value is :%d\n", div=x/y);
+  scanf("%d", &div);
+  return 0;
+}
