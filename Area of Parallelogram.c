baa9a4976e2d3a5760bd0e8474fdbd81a4b7296a
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 19:58:27 +0530

    Create Area of Parallelogram.c

---
+++ Area of Parallelogram.c
@@ -0,0 +1,8 @@
+float main() {
+    float base,height,area;
+    printf("enter the base and height:");
+    scanf("%f %f",&base,&height);
+    area=(base*height);
+    printf("The area is%f",area);
+    return 0;
+}
