9f1a5263be42e4f6220371b939631c1507f62116
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 12:37:34 +0530

    Create Area Perimeter of Circle.c

---
+++ Area Perimeter of Circle.c
@@ -0,0 +1,8 @@
+float main() {
+    float radius,perimeter;
+    printf("enter the radius:");
+    scanf("%f",&radius);
+    perimeter=2.0*3.14*radius;
+    printf("The perimeter is%f",perimeter);
+    return 0;
+}
