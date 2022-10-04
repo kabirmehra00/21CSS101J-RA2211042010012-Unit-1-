ddb4d058bbdcf8b6a4a65066ad6bcf0762948fc5
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 12:40:47 +0530

    Create Area of Rectangle.c

---
+++ Area of Rectangle.c
@@ -0,0 +1,8 @@
+float main() {
+    float length,breadth,area;
+    printf("enter the length and breadth:");
+    scanf("%f %f",&length,&breadth);
+    area=length*breadth;
+    printf("The area is%f",area);
+    return 0;
+}
