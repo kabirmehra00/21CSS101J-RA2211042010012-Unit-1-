961c723f8927275aa109b8c9f9c9c9eaeb824ff2
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 20:01:52 +0530

    Create Perimeter of Triangle.c

---
+++ Perimeter of Triangle.c
@@ -0,0 +1,8 @@
+float main() {
+    float side1,side2,side3,perimeter;
+    printf("enter the 3 sides:");
+    scanf("%f %f %f",&side1,&side2,&side3);
+    perimeter=side1+side2+side3;
+    printf("The perimeter is%f",perimeter);
+    return 0;
+}
