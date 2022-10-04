1cb0ee9630183617f7a07742c87ee8d0710f03e5
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 20:36:24 +0530

    Create Intro.c

---
+++ Intro.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("SRM IST\n");
+  printf("Mridul Mehra\n");
+  printf("B.Tech in Computer Science and Buissness Systems\n");
+  printf("RA2211042010012\n");
+  
+  return 0;
+}
