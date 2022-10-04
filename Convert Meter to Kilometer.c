7cc615be00e3f97432ede6e6aa4aff7ace197c99
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 12:17:37 +0530

    Create Convert Meter to Kilometer.c

---
+++ Convert Meter to Kilometer.c
@@ -0,0 +1,8 @@
+float main() {
+    float kilometre,metre;
+    printf("enter the distance in metre:");
+    scanf("%f",&metre);
+    kilometre=metre/1000;
+    printf("the distance in kilometre:%f",kilometre);
+    return 0;
+}
