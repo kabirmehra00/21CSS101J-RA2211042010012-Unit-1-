6351ac904b3be9690ba97ecbf1540548488d7f54
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 12:05:50 +0530

    Update Subtract.c

--- Subtract.c
+++ Subtract.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
-
-int main(){
-    int x , y;
-    int result = x + y;
-    printf("Enter the value of x :\n",x );
-    scanf("%d", &x);
-    printf("Enter the value of x :\n",y );
-    scanf("%d", &y);
-    printf("The required sum is :%d\n", result = x + y);
-    scanf("%d", &sum);
-    return 0;
+main()
+{
+    int a,b,c;
+    a = 5;
+    b = 10;
+    c = a-b ;
+    printf("Subtraction of %d and %d is %d",a,b,c);
 }
