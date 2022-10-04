4e0169c46ccdc799b772fb576fa3923e59d9e4c3
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 20:05:01 +0530

    Create Perimeter of Parallelogram.c

---
+++ Perimeter of Parallelogram.c
@@ -0,0 +1,8 @@
+float main() {
+    float side1,side2,perimeter;
+    printf("enter the 2 sides:");
+    scanf("%f %f",&side1,&side2);
+    perimeter=2*(side1+side2);
+    printf("The perimeter is%f",perimeter);
+    return 0;
+}
