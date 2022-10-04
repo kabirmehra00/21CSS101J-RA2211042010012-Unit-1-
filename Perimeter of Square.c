9f9963a8accf172f413e276fd0c063442256f632
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-04 19:49:09 +0530

    Create Perimeter of Square.c

---
+++ Perimeter of Square.c
@@ -0,0 +1,8 @@
+float main() {
+    float side,perimeter;
+    printf("enter the side:");
+    scanf("%f",&side);
+    perimeter=4*(side);
+    printf("The perimeter is%f",perimeter);
+    return 0;
+}
