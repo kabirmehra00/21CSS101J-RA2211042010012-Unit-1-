30f386f3b992cb28f3cf0fe42730382c6dcb2954
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-05 19:03:52 +0530

    Create Convert Inch to Centimeters.c.c

---
+++ Convert Inch to Centimeters.c.c
@@ -0,0 +1,10 @@
+int main()
+{
+   float inchesHeight, centiHeight;
+   printf("Enter Person Height in inches =");
+   scanf("%f", &inchesHeight);
+   
+   centiHeight = inchesHeight * 2.54;
+   printf("Height of Person in Centimeters =%.2f", centiHeight);
+   return 0;
+}
