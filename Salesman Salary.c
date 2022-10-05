5f74e2288d8ecf703833cea135f14ce489e42340
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-05 22:16:35 +0530

    Create Salesman Salary.c

---
+++ Salesman Salary.c
@@ -0,0 +1,17 @@
+#define BASIC_SALARY 3000.00
+#define BONUS_RATE 200.00
+#define COMMISSION 0.05
+main(){
+   int quantity ;
+   float gross_salary, price ;
+   float bonus, commission ;
+   printf("number of items sold and their price\n") ;
+   scanf("%d %f", &quantity, &price) ;
+   bonus = BONUS_RATE * quantity ;
+   commission = COMMISSION * quantity * price ;
+   gross_salary = BASIC_SALARY + bonus + commission ;
+   printf("\n");
+   printf("Bonus = %6.2f\n", bonus) ;
+   printf("Commission = %6.2f\n", commission) ;
+   printf("Gross salary = %6.2f\n", gross_salary) ;
+}
