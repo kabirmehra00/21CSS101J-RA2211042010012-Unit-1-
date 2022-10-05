b82c2fa38731d50c8324e58b4ad9e552c19999ed
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-05 21:53:56 +0530

    Create How many digits.c

---
+++ How many digits.c
@@ -0,0 +1,10 @@
+int main()
+{
+    int number;
+    printf("Enter an integer number\n");
+    scanf("%d",&number);
+    if ( number < 100 )
+    printf("Your no, is smaller than 100\n\n");
+    else
+    printf("Your no. contains more than 2 digits\n\n");
+}
