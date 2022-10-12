3d37448e78d8b5516fc300a6e448f4c6395c6ebb
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-12 09:52:11 +0530

    Update Area of Triangle.c

--- Area of Triangle.c
+++ Area of Triangle.c
@@ -1,5 +1,6 @@
 float main() {
     float base,height,area;
+    printf("RA2211042010012\n");
     printf("enter the base and height:");
     scanf("%f %f",&base,&height);
     area=0.5*(base*height);
