82a8a97cdaac5e3f020d3392722c53181d2816dd
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-12 09:51:34 +0530

    Update Area of Rectangle.c

--- Area of Rectangle.c
+++ Area of Rectangle.c
@@ -1,5 +1,6 @@
 float main() {
     float length,breadth,area;
+    printf("RA2211042010012\n");
     printf("enter the length and breadth:");
     scanf("%f %f",&length,&breadth);
     area=length*breadth;
