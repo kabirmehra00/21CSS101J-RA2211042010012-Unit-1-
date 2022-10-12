625440f8d7f3eee5fe90333d7dc00f9b30d500b6
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-12 09:52:59 +0530

    Update Convert Celcuis to Farenheit.c

--- Convert Celcuis to Farenheit.c
+++ Convert Celcuis to Farenheit.c
@@ -1,5 +1,6 @@
 float main() {
     float farenheit,celcius;
+    printf("RA2211042010012\n");
     printf("enter the temprature in celcius:");
     scanf("%f",&celcius);
     farenheit=((9.0/5.0*celcius)+32.0);
