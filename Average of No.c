baaa2105eae323f425daa8c04ec8e5f3cc26b2ed
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-12 09:52:40 +0530

    Update Average of No.c

--- Average of No.c
+++ Average of No.c
@@ -2,7 +2,7 @@ int main()
 {
     int n, i;
     float sum = 0, x;
-
+    printf("RA2211042010012\n");
     printf("Enter number of elements:  ");
     scanf("%d", &n);
     printf("\nEnter %d elements\n", n);
