db36d0d4694b52538c5f76fe06316478f645b73f
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:50:06 +0530

    Update Logical Operators(AND).c

--- Logical Operators(AND).c
+++ Logical Operators(AND).c
@@ -2,12 +2,10 @@
 int main()
 {
     int a = 1, b = 1, c = 2, result;
-
+    printf("RA2211042010012\n");
     result = (a == b) && (c > b);
     printf("(a == b) && (c > b) is %d \n", result);
-
     result = (a == b) && (c < b);
     printf("(a == b) && (c < b) is %d \n", result);
-
     return 0;
 }
