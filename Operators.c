e3033a5a7d53cac6e9a707fd0092c272b6116852
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:53:54 +0530

    Update Operators.c

--- Operators.c
+++ Operators.c
@@ -4,6 +4,7 @@ int main()
     a = 15;
     b = 10;
     c = ++a - b;
+    printf("RA2211042010012\n");
     printf("a = %d\n b = %d\n c = %d\n",a,b,c);
     d = b++ +a;
     printf("a = %d\n b = %d\n d = %d\n",a,b,d);
