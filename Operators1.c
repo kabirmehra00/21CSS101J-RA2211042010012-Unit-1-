ce34ab8e9c3e9965fc930aad37afdb51f4c0c283
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:54:38 +0530

    Update Operators1.c

--- Operators1.c
+++ Operators1.c
@@ -1,11 +1,9 @@
 int main() {
     int a = 5, c;
-  
-     printf("Use of +=\n");
-  
+    printf("RA2211042010012\n");
+    printf("Use of +=\n");
     c = a;     
     printf("c = %d\n", c);
     c += a;    
-    
     return 0;
 }
