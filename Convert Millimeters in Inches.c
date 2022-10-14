5962c6608c81963af38002c7015cde5406ebb55d
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:40:50 +0530

    Update Convert Millimeters in Inches.c

--- Convert Millimeters in Inches.c
+++ Convert Millimeters in Inches.c
@@ -1,10 +1,11 @@
 int main()
 {
 double mm, inches;
-printf(“Enter millimetrs to be converted into Inches:”);
-scanf(“%lf”, &mm);
-inches = mm * 0.03937007874;
-printf(“Inches are=%f”,inches);
-return 0;
+    printf("RA2211042010012\n");
+  printf(“Enter millimetrs to be converted into Inches:”);
+  scanf(“%lf”, &mm);
+  inches = mm * 0.03937007874;
+  printf(“Inches are=%f”,inches);
+  return 0;
 }
 
