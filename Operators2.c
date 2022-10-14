c9e0d8b544e1652c3c08bd4599a6ef93e3a2658d
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:57:11 +0530

    Update Operators2.c

--- Operators2.c
+++ Operators2.c
@@ -1,11 +1,10 @@
 int main() {
+    printf("RA2211042010012\n");
     int a = 6, c;
-  
-     printf("Use of -=\n");
-  
+    printf("Use of -=\n");  
     c = a;     
     printf("c = %d\n", c);
-    c -= a;    
-    
+    c -= a;  
+    printf("c -= %d\n", c);
     return 0;
 }
