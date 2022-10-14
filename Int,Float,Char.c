b86e3fa9d299b42b9811cb3f79de69cc2da210c2
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:47:35 +0530

    Update Int,Float,Char.c

--- Int,Float,Char.c
+++ Int,Float,Char.c
@@ -3,7 +3,7 @@ main()
     int myNum = 5;
     float myFloatNum = 5.99;
     char myLetter = 'D';
-  
+    printf("RA2211042010012\n");
     printf("%d\n", myNum);
     printf("%f\n", myFloatNum);
     printf("%c\n", myLetter);
