f4566722881696b3c0ee7545d88c43d53db5ce7e
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:52:25 +0530

    Update N to the power P.c

--- N to the power P.c
+++ N to the power P.c
@@ -1,19 +1,14 @@
 int main()
 {
     int n, i, p;
-	
+	printf("RA2211042010012\n");
 	long power=1;
-    
 	printf("Enter the number N=");
 	scanf("%d", &n);
-	
 	printf("Enter the Power P=");
 	scanf("%d", &p);
-	
 	for(i=1;i<=p;i++)
 	  power = power * n;
-	
 	printf("The number %d raised to the power %d is %ld", n, p, power);
-	
     return 0;
 }
