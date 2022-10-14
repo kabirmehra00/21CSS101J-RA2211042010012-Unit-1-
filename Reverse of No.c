734b64c7250f1376f3257c1fe453b4ef22797094
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:58:26 +0530

    Update Reverse of No.c

--- Reverse of No.c
+++ Reverse of No.c
@@ -1,6 +1,7 @@
  int main()    
 {    
-int n, reverse=0, rem;    
+int n, reverse=0, rem;
+  printf("RA2211042010012\n");
 printf("Enter a number: ");    
   scanf("%d", &n);    
   while(n!=0)    
