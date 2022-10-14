9d7059c93435e761e4d2f2e37e2d2a2386103396
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-15 03:49:36 +0530

    Update Largest Integer.c

--- Largest Integer.c
+++ Largest Integer.c
@@ -1,23 +1,14 @@
 int main() 
-{ 
-     
-    int n1, n2, n3, max; 
-      
-     
+{
+    int n1, n2, n3, max;
+	printf("RA2211042010012\n");
     printf("Enter first number=");
 	scanf("%d",&n1);
-	
-	
 	printf("Enter second number=");
 	scanf("%d",&n2);
-	
 	printf("Enter third number=");
 	scanf("%d",&n3);
-	
 	max = (n1 > n2) ? (n1 > n3 ? n1 : n3) : (n2 > n3 ? n2 : n3); 
-      
-     
     printf("Largest number among %d, %d and %d is %d.",n1, n2, n3, max); 
-  
     return 0; 
 } 
