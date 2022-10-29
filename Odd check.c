48c90cfcc4673acd47f6fefefa8df8644473a900
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-10-30 00:27:35 +0530

    Create Odd check.c

---
+++ Odd check.c
@@ -0,0 +1,11 @@
+int main()
+{
+    int number;
+  printf("RA2211042010012\n");
+    printf("Enter the Number:");
+    scanf("%d",&number);
+    if(number%2==1)
+    {
+        printf("The number is odd");
+    }
+}
