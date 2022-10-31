31d873ab31f4103ab31a2156fb58c3323d635b1a
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-01 00:13:43 +0530

    Create If Else Ladder.c

---
+++ If Else Ladder.c
@@ -0,0 +1,18 @@
+void main() 
+{
+	int a;
+	printf("Enter a Number: ");
+	scanf("%d",&a);
+	if(a > 0)
+	{
+		printf("Given Number is Positive"); 
+	}
+	else if(a == 0)
+	{
+	    printf("Given Number is Zero");
+	}
+	else if(a < 0)
+	{ 
+		printf("Given Number is Negative"); 
+	}
+}
